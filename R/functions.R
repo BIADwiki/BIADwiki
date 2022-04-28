81ab8a9a784f0d6354d05f36cc9edc504bec06a2
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-04-28 18:40:44 +0100

    DB connection test

---
+++ R/functions.R
@@ -0,0 +1,28 @@
+#--------------------------------------------------------------------------------------------------
+encoder <- function(df){
+	if(nrow(df)==0) return(NULL)
+	if(nrow(df)>0){
+		for(n in 1:ncol(df))
+		if(class(df[,n])=="character")df[,n] <- iconv(df[,n],"utf8","utf8")
+		return(df)	
+		}
+	}
+#--------------------------------------------------------------------------------------------------
+sql.wrapper <- function(sql.command,user,password){
+	require(RMySQL)
+	require(odbc)
+	drv <- dbDriver("MySQL")
+
+	# connect locally to the database
+	con <- dbConnect(drv,host = "127.0.0.1", user=user, pass=password)
+	dbSendQuery(con,"SET NAMES 'utf8'")
+
+	# query the database and tidy
+	res <- dbSendQuery(con,sql.command)
+	query <- fetch(res, n= -1)
+	query <- encoder(query)
+
+	# close the connection to the database
+	suppressWarnings(dbDisconnect(con))
+return(query)}
+#--------------------------------------------------------------------------------------------------
