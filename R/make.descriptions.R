9185639879ebcb386da5fdad3dda1ca0407cc2e0
Adrian Timpson <a.timpson@ucl.ac.uk>
2022-05-04 21:32:38 +0100

    auto update from server

--- R/make.descriptions.R
+++ R/make.descriptions.R
@@ -5,6 +5,7 @@ d <- sql.wrapper(sql.command,user,password)
 #------------------------------------------------------------------
 # Write to the Gist
 writeLines(d[1,1], con='../../Gists/Phases/Phases.md')
+rm('../../Gists/Phases/.DS_Store')
 #------------------------------------------------------------------
 
 	
