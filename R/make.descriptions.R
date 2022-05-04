124a6c30df9d68788a423961c09c67ee38e13fed
Adrian Timpson <a.timpson@ucl.ac.uk>
2022-05-04 21:48:41 +0100

    auto update from server

--- R/make.descriptions.R
+++ R/make.descriptions.R
@@ -1,11 +1,17 @@
 
 #------------------------------------------------------------------
+# Pull table summaries from the dtabase, and update to Gists
+# Should be able to loop it
+#------------------------------------------------------------------
+# Sites
+sql.command <- "SELECT table_comment FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='COREX' AND table_name='Sites';"
+d <- sql.wrapper(sql.command,user,password)
+writeLines(d[1,1], con='../../Gists/Sites/Sites.md')
+#------------------------------------------------------------------
+# Phases
 sql.command <- "SELECT table_comment FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='COREX' AND table_name='Phases';"
 d <- sql.wrapper(sql.command,user,password)
-#------------------------------------------------------------------
-# Write to the Gist
 writeLines(d[1,1], con='../../Gists/Phases/Phases.md')
-rm('../../Gists/Phases/.DS_Store')
 #------------------------------------------------------------------
 
 	
