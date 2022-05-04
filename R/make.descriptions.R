b5d2f77970b32bf13a5db8f61b47b5c5c39bb5b3
Adrian Timpson <a.timpson@ucl.ac.uk>
2022-05-04 19:56:56 +0100

    restructure for multiple scripts

--- R/make.descriptions.R
+++ R/make.descriptions.R
@@ -1,9 +1,10 @@
 
 #------------------------------------------------------------------
-"SELECT table_comment FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='COREX' AND table_name='Phases';"
+sql.command <- "SELECT table_comment FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='COREX' AND table_name='Phases';"
 d <- sql.wrapper(sql.command,user,password)
 #------------------------------------------------------------------
-
+# Write to the Gist
+writeLines(d[1,1], con='../../Gist/Phases.md')
 #------------------------------------------------------------------
 	
 	
