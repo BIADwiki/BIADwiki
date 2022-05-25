4d2e48bb1ce1d77addf9f4854b2f4c5fe2f8b9d3
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-05-25 12:07:05 +0100

    auto update from server

--- R/controller.R
+++ R/controller.R
@@ -1,10 +1,10 @@
 #------------------------------------------------------------------
-# Runs all R scripts beginning with 'make.'
+# Runs all R scripts beginning with 'auto.make.'
 #------------------------------------------------------------------
 source('functions.R')
 #------------------------------------------------------------------
 files <- list.files()
-files_to_run <- files[substr(files,1,5)=='make.']
+files_to_run <- files[substr(files,1,10)=='auto.make.']
 N <- length(files_to_run)
 for(n in 1:N){
 	print(date())
