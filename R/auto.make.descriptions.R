401dbb5b22babef9ba6b44e00d553c7927e2cd92
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-05-25 14:52:26 +0100

    Update auto.make.descriptions.R

--- R/auto.make.descriptions.R
+++ R/auto.make.descriptions.R
@@ -13,13 +13,12 @@ d.cols <- sql.wrapper(sql.command,user,password)
 
 # Group tables to put into different parts of the wiki:
 all <- d.tables$TABLE_NAME
-ztemp <- all[grepl('ztemp', all)]
+zprivate <- all[grepl('zprivate', all)]
 zoptions <- all[grepl('zoptions', all)]
 copy <- all[grepl('_copy', all)]
-standard <- all[!all%in%c(ztemp,zoptions,copy)]
+standard <- all[!all%in%c(zprivate,zoptions,copy)]
 
-# construct a single markdown file for all standard tables
+# construct a single markdown file for all standard tables and zoptions
 create.markdown.for.several.tables(d.tables, d.cols, table.names = standard, file = '../../Gists/table_comments/standard/standard.md')
 create.markdown.for.several.tables(d.tables, d.cols, table.names = zoptions, file = '../../Gists/table_comments/zoptions/zoptions.md')
-create.markdown.for.several.tables(d.tables, d.cols, table.names = ztemp, file = '../../Gists/table_comments/ztemp/ztemp.md')
 #-----------------------------------------------------------------------------------------
