9c8c49121f74198152643f4f39805a47bbc509ea
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-06-24 14:51:37 +0100

    auto update from server

--- R/controller.R
+++ R/controller.R
@@ -1,16 +1,34 @@
 #------------------------------------------------------------------
 # Runs all R scripts beginning with 'auto.make.'
+# This script is invoked by a regular CRON schedule (Admin),
+# However, some scripts need not be run so frequently.
+# Rather than specifying a CRON schedule for each script, 
+# this script decides when to run which script, based on the filename.
 #------------------------------------------------------------------
 source('functions.R')
 #------------------------------------------------------------------
+run.day <- 'Mon'
+today <- strsplit(date(),split=' ')[[1]][1]
+
 files <- list.files()
-files_to_run <- files[substr(files,1,10)=='auto.make.']
-N <- length(files_to_run)
-for(n in 1:N){
-	print(date())
-	source(files_to_run[n])
+daily.files <- files[grepl('auto.make.daily',files)]
+weekly.files <- files[grepl('auto.make.weekly',files)]
+
+ND <- length(daily.files)
+NW <- length(weekly.files)
+
+if(ND>0)for(n in 1:ND){
+	file <- daily.files[n]
+	print(paste('Starting to run script:',file,'at',date())
+	source(file)
 	}
-date()
+
+if(NW>0 & today==run.day)for(n in 1:NW){
+	file <- weekly.files[n]
+	print(paste('Starting to run script:',file,'at',date())
+	source(file)
+	}
+
 #------------------------------------------------------------------
 	
 	
