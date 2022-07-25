9c3cc56f9486c48d80b4957ef42adbbc23ec7a74
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-07-25 15:34:20 +0100

    test

--- R/auto.make.daily.robertsapples.R
+++ R/auto.make.daily.robertsapples.R
@@ -8,11 +8,9 @@ d$userstamp <- '\\N'
 
 library(gridExtra)
 
-mytheme <- gridExtra::ttheme_default(
-    core = list(fg_params=list(cex = 1.5)),
-    colhead = list(fg_params=list(cex = 1.5)),
-    rowhead = list(fg_params=list(cex = 1.5)))
 
-png('../tools/plots/apples.png',width=1600, height=400)
+mytheme <- ttheme_default(base_size=20)
+
+png('../tools/plots/apples.png',width=2000, height=400)
 grid.table(d, theme=mytheme)
 dev.off()
