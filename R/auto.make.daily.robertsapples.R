87fe96dad9359ee11c1479e25f74693d956c32eb
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-07-25 15:15:53 +0100

    test

--- R/auto.make.daily.robertsapples.R
+++ R/auto.make.daily.robertsapples.R
@@ -1,4 +1,4 @@
-library(gridExtra)
+
 sql.command <- "SELECT * FROM BIAD.Sites"
 d <- sql.wrapper(sql.command,user,password,hostname,hostuser,keypath,ssh)
 d <- d[1:5,]
@@ -6,7 +6,7 @@ d[is.na(d)] <- '\\N'
 d$timestamp <- '\\N'
 d$userstamp <- '\\N'
 
-
-png('../tools/plots/apples.png',width=800, height=200)
+library(gridExtra)
+png('../tools/plots/apples.png',width=1600, height=400)
 grid.table(d)
 dev.off()
