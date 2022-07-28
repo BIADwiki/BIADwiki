dbaddb282a07b272d28a12d8a4a28836ba3ff069
bobexer <rstaniuk@gmail.com>
2022-07-28 14:26:56 +0100

    Updated figures and codes

--- R/auto.make.daily.citations.R
+++ R/auto.make.daily.citations.R
@@ -1,13 +1,14 @@
+source('functions.R')
 sql.command <- "SELECT * FROM BIAD.Citations"
 d <- sql.wrapper(sql.command,user,password,hostname,hostuser,keypath,ssh)
-d <- d[1:20,]
+d <- d[1:5,]
 d[is.na(d)] <- '\\N'
 d$timestamp <- '\\N'
 d$userstamp <- '\\N'
 
 library(gridExtra)
 
-png('../tools/plots/citations.png',width=1000, height=350)
+png('../tools/plots/citations.png',width=4000, height=350)
 grid.table(d, theme=ttheme_minimal())
 dev.off()
 
