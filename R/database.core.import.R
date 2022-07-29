f4aab9b14073e83d33551142178a555f5a240e15
bobexer <rstaniuk@gmail.com>
2022-07-29 14:48:57 +0100

    Update database.core.import.R

--- R/database.core.import.R
+++ R/database.core.import.R
@@ -2,53 +2,36 @@
 # Example R script for relationships used in core import
 #------------------------------------------------------------------
 # Created: 28.07.2022
-# Last modified: 28.07.2022
+# Last modified: 29.07.2022
 
 library(DiagrammeR)
 core.import <- DiagrammeR::grViz("
 digraph {
-#data tables
-  node [shape = circle
-  style = filled,
-  color = orange,
-  fixedsize = true,
-  width = 2.2,
-  fontsize = 15]
-  Citations; Sites; Phases; PhaseCitation; PhaseType
-
-#list tables
-  node [shape = box
-  style = filled,
-  color = lightblue
-  fixedsize = true,
-  width = 3.0
-  fontsize = 15]
-
-  edge [color = dimgray]  
-Citations -> Sites -> Phases
-Citations -> PhaseCitation -> Phases
-zoptions_Types -> PhaseType -> Phases
-zoptions_Countries -> Sites
-zoptions_Cultures -> Phases
-zoptions_Periods -> Phases
-subgraph cluster {
-node [shape = circle
-  style = filled,
-  color = orange,
-  fixedsize = true,
-  width = 1,
-  fontsize = 10]
-  DataTable
-  node [shape = box
-  style = filled,
-  color = lightblue
-  fixedsize = true,
-  width = 1
-  fontsize = 10]
-  List
+ node [shape = record];
+{
+node [shape = rectangle style = filled fillcolor = white]
+Citations Sites
+}
+{
+node [shape = rectangle style = filled fillcolor = white]
+Phases PhaseCitation PhaseType
+}
+subgraph {
+{rank=same Sites Citations}
+Sites -> Phases [dir = back]
+Citations -> PhaseCitation [dir = back];
+}
+subgraph {
+{rank=same PhaseCitation PhaseType Phases}
+PhaseCitation -> Phases [dir = back]
+}
+subgraph {
+{rank=same  Phases}
+Phases -> PhaseType;
 }
 }
 ")
+core.import
 export_svg(core.import) %>%
   charToRaw %>%
   rsvg_png("../tools/plots/database.core.import.png")
