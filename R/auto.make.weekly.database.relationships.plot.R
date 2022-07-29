491a92dbd9778efdaed0a7d3b3d08e8255ae27e5
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-07-29 12:37:32 +0100

    automated from MySQL

---
+++ R/auto.make.weekly.database.relationships.plot.R
@@ -0,0 +1,79 @@
+#------------------------------------------------------------------
+# Example R script for plotting database relationships between tables
+#------------------------------------------------------------------
+# Created: 28.07.2022
+# Last modified: 28.07.2022
+#------------------------------------------------------------------
+# Pull all foreign keys
+#------------------------------------------------------------------
+source('functions.R')
+
+sql.command <- "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='BIAD';"	
+d.tables <- sql.wrapper(sql.command,user,password,hostname,hostuser,keypath,ssh)$TABLE_NAME	
+
+zprivate <- d.tables[grepl('zprivate', d.tables)]
+zoptions <- d.tables[grepl('zoptions', d.tables)]
+standard <- d.tables[!d.tables%in%c(zoptions,zprivate)]
+
+sql.command <- "SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_SCHEMA = 'BIAD'"
+d <- sql.wrapper(sql.command,user,password,hostname,hostuser,keypath,ssh)
+#------------------------------------------------------------------
+# get data tables
+tables <- paste(standard, collapse='; ')
+
+data.tables <- paste("
+  node [shape = circle
+  style = filled,
+  color = orange,
+  fixedsize = true,
+  width = 2.2,
+  fontsize = 15]
+"
+, tables, sep='\n ')
+#------------------------------------------------------------------
+# get lookup tables
+tables <- zoptions
+
+look.ups <- paste("
+  node [shape = box
+  style = filled,
+  color = lightblue
+  fixedsize = true,
+  width = 3.0
+  fontsize = 15]
+"
+, tables, sep='\n ')
+#------------------------------------------------------------------
+# convert foreign keys into a suitable format for DiagrammeR
+edges <- paste(d$REFERENCED_TABLE_NAME, d$TABLE_NAME, sep=' -> ')
+edges <- paste('edge [color = dimgray]', edges, collapse='\n ')
+#------------------------------------------------------------------
+subgraph <- "
+subgraph cluster {
+node [shape = circle
+  style = filled,
+  color = orange,
+  fixedsize = true,
+  width = 1,
+  fontsize = 10]
+  DataTable
+  node [shape = box
+  style = filled,
+  color = lightblue
+  fixedsize = true,
+  width = 1
+  fontsize = 10]
+  LookUpTable
+}
+"
+#------------------------------------------------------------------
+library(DiagrammeR)
+diagram <- paste("digraph {", data.tables, look.ups, edges, subgraph, "}")
+image <- DiagrammeR::grViz(diagram)
+
+export_svg(image) %>%
+  charToRaw %>%
+  rsvg_png("../tools/plots/database.relationships.plot.png")
+#------------------------------------------------------------------
+
+
