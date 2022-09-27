a6e011546f7f7ef6a0d5164f4aec6d194d3fc570
AdrianTimpson <a.timpson@ucl.ac.uk>
2022-09-27 12:22:57 +0100

    Update sandpit.R

--- R/sandpit.R
+++ R/sandpit.R
@@ -84,41 +84,26 @@ unique(new$GraveID[!new$GraveID%in%gra$GraveID])
 nchar(new$CitationID)
 unique(new$CitationID)
 #-----------------------------------------------------------------------------------------
-# merge FaunalBones and FaunalBiometrics ino a new FaunalBones
+# merge faunal taxa and abot taxa into a single table
 #-----------------------------------------------------------------------------------------
-bo <- sql.wrapper("SELECT * FROM BIAD.FaunalBones",user,password,hostname,hostuser,keypath,ssh)
-bi <- sql.wrapper("SELECT * FROM BIAD.FaunalBiometrics",user,password,hostname,hostuser,keypath,ssh)
-both <- merge(bo,bi,by='BoneID')
-newID <- both$MetricID+9000000
-newID <- as.character(newID)
-newID <- paste('M',substring(newID,2),sep='')
-stamps <- data.frame(time_added=rep(NA,nrow(both)), user_added=rep(NA,nrow(both)), time_last_update=both$timestamp.x, user_last_update=both$userstamp.x)
-castrate <- data.frame(Castrate=NA)
-new <- cbind(data.frame(MetricID=newID),both[,c('BoneID','PhaseID','TaxonCode','Element','Sex')],castrate,both[,c('Measurement','Value')],stamps)
+ft <- sql.wrapper("SELECT * FROM `BIAD`.`zoptions_FaunalTaxaList`",user,password,hostname,hostuser,keypath,ssh)
+at <- sql.wrapper("SELECT * FROM `BIAD`.`zoptions_ABotTaxaList`",user,password,hostname,hostuser,keypath,ssh)
+names(at)[5] <- 'WildDomesticStatus'
+ft <- ft[,1:8]
+ft <- cbind(ft, data.frame(Kingdom='Animal'))
+at <- cbind(at, data.frame(Kingdom ='Plant'))
+require(dplyr)
+both <- bind_rows(ft,at)
+table(both$WildDomesticStatus)
+write.csv(both,file='toadd.csv',fileEncoding = "UTF-8",row.names=F, na='\\N')
+#-----------------------------------------------------------------------------------------
+tax <- sql.wrapper("SELECT * FROM `BIAD`.`zoptions_TaxaList`",user,password,hostname,hostuser,keypath,ssh)
 
-i <- new$Sex=='Undet'
-new$Sex[i] <- 0.5; new$Castrate[i] <- NA
-i <- new$Sex=='Castrate'
-new$Sex[i] <- 0; new$Castrate[i] <- 1
-i <- new$Sex=='Castrate?'
-new$Sex[i] <- NA; new$Castrate[i] <- 0.5
-i <- new$Sex=='Male'
-new$Sex[i] <- 0; new$Castrate[i] <- 0
-i <- new$Sex=='Male?'
-new$Sex[i] <- 0.25; new$Castrate[i] <- NA
-i <- new$Sex=='Female'
-new$Sex[i] <- 1; new$Castrate[i] <- 0
-i <- new$Sex=='Female?'
-new$Sex[i] <- 0.75; new$Castrate[i] <- NA
+mt <- sql.wrapper("SELECT * FROM `BIAD`.`MaterialCulture`",user,password,hostname,hostuser,keypath,ssh)
+x <- unique(mt$TaxonCode)
+x[!x%in%tax$TaxonCode]
+table(mt$TaxonCode)
+unique(tax$TaxonCode)
 
-sql <- c()
-for(n in 1:nrow(new)){
-	i <- !is.na(new[n,])
-	names <- paste(names(new)[i],collapse="`, `")
-	values <- paste(new[n,i], collapse="', '")
-	sql[n] <- paste("INSERT INTO `BIAD`.`FaunalAdrian` (`",names, "`) VALUES ('", values, "')",sep="")
-	}
-
-sql.wrapper(sql,user,password,hostname,hostuser,keypath,ssh)
 #-----------------------------------------------------------------------------------------
 
