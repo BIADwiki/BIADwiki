0431ffca19dfed9d324f3cdb6c8b49a4cd934c8a
AdrianTimpson <a.timpson@ucl.ac.uk>
2023-01-27 10:08:04 +0000

    size

--- R/auto.make.daily.database.relationships.plot.R
+++ R/auto.make.daily.database.relationships.plot.R
@@ -22,5 +22,5 @@ export_svg(image) %>% charToRaw %>% rsvg_png("../tools/plots/database.relationsh
 # set 1
 d.tables <- paste(c('Sites','Phases','C14Samples','Graves','FaunalIsotopes','ABotPhases','StrontiumEnvironment'), collapse='; ')
 image <- database.relationship.plotter(d.tables, FALSE)
-export_svg(image) %>% charToRaw %>% rsvg_png("../tools/plots/database.relationships.plot.set.1.png", height=400)
+export_svg(image) %>% charToRaw %>% rsvg_png("../tools/plots/database.relationships.plot.set.1.png", height=200)
 #------------------------------------------------------------------
