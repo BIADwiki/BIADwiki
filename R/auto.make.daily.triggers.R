f67c70aefba8d3e153f63ae3684e5db2b261ea0c
AdrianTimpson <a.timpson@ucl.ac.uk>
2023-06-03 15:27:24 +0100

    Update auto.make.daily.triggers.R

--- R/auto.make.daily.triggers.R
+++ R/auto.make.daily.triggers.R
@@ -4,7 +4,7 @@
 #--------------------------------------------------------------------------------------------------------------
 tables <- query.database(user, password, "SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE TABLE_SCHEMA='biad' AND `TABLE_TYPE`='BASE TABLE';")
 all <- query.database(user, password, "SELECT `TABLE_NAME`, `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE TABLE_SCHEMA='biad';")
-trig <- query.database(user, password, "SHOW TRIGGERS;",user,password,hostname,hostuser,keypath,ssh)$Trigger
+trig <- query.database(user, password, "SHOW TRIGGERS;")$Trigger
 
 all <- subset(all, TABLE_NAME%in%tables$TABLE_NAME)
 old.triggers <- trig[substr(trig, 1, 20) =='auto_trigger_padding']
