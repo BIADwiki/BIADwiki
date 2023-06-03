22562ca6e35d380ecb162371ccc87d50800f7e87
AdrianTimpson <a.timpson@ucl.ac.uk>
2023-06-03 15:24:03 +0100

    Update auto.make.daily.triggers.R

--- R/auto.make.daily.triggers.R
+++ R/auto.make.daily.triggers.R
@@ -2,9 +2,9 @@
 # Create MySQL triggers to ensure all leading and trailing whitespace is removed for any insert or update.
 # Needs recreating regulary, as table structures change over time.
 #--------------------------------------------------------------------------------------------------------------
-tables <- sql.wrapper("SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE TABLE_SCHEMA='biad' AND `TABLE_TYPE`='BASE TABLE';",user,password,hostname,hostuser,keypath,ssh)
-all <- sql.wrapper("SELECT `TABLE_NAME`, `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE TABLE_SCHEMA='biad';",user,password,hostname,hostuser,keypath,ssh)
-trig <- sql.wrapper("SHOW TRIGGERS;",user,password,hostname,hostuser,keypath,ssh)$Trigger
+tables <- query.database(user, password, "SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE TABLE_SCHEMA='biad' AND `TABLE_TYPE`='BASE TABLE';")
+all <- query.database(user, password, "SELECT `TABLE_NAME`, `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE TABLE_SCHEMA='biad';")
+trig <- query.database(user, password, "SHOW TRIGGERS;",user,password,hostname,hostuser,keypath,ssh)$Trigger
 
 all <- subset(all, TABLE_NAME%in%tables$TABLE_NAME)
 old.triggers <- trig[substr(trig, 1, 20) =='auto_trigger_padding']
