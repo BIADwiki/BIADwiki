b4febffa387c45a9147c0035ccb4f80c16cb344b
AdrianTimpson <a.timpson@ucl.ac.uk>
2023-06-05 23:28:01 +0100

    minor fiddling

--- R/server.script.R
+++
@@ -1,5 +0,0 @@
-source('functions.R')
-source('.Rprofile')
-sql.command <- c("SELECT `C14ID`,`C14Samples`.`PhaseID`,`C14Samples`.`SiteID`,`C14Samples`.`Period`,`C14.Age`,`C14.SD`,`LabID`,`Culture1` FROM `C14Samples` INNER JOIN `Phases` ON `C14Samples`.`PhaseID`=`Phases`.`PhaseID` WHERE `Culture1`='Bell Beaker'")
-query <- query.database(user, password, sql.command)
-save(query, file='tmp.RData')
