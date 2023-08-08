6b97ebebbbcba0e31b0ce3115feb14692ad67c92
bobexer <rstaniuk@gmail.com>
2023-08-08 13:12:53 +0100

    Update example.coordinate_conversion.R

--- R/example.coordinate_conversion.R
+++ R/example.coordinate_conversion.R
@@ -11,7 +11,9 @@ head(___) # inspect the file
 
 # convert the data.frame to an sf object
 # pay attention to the re-arranged coordinate columns
-# input the crs value for the original porjection
+# input the crs value for the original projection
+names(___)[names(___) == "x"] <- "E"
+names(___)[names(___) == "y"] <- "N"
 ___ <- st_as_sf(___, coords = c("E", "N"), crs = ___)  
 
 # inspect the file
@@ -23,5 +25,11 @@ ___ <- st_transform(___, crs = 4326)
 # inspect the file
 head(___) 
 
-#save the file as .csv ready for import
+# check the coordinates with Google Maps (copy-paste the geometry) to see whether:
+# 1. it is projected correctly
+# 2. has inverted coordinates
+# if the coordinates are inverse, make sure that the export line (31) includes the statement
+# layer_options = "GEOMETRY=AS_YX". If not, use layer_options = "GEOMETRY=AS_XY".
+
+# save the file as .csv ready for import
 st_write(___, "___.csv", layer_options = "GEOMETRY=AS_YX", delete_dsn = TRUE)
