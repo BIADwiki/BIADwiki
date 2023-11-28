0475d8d9cf9c01d87b42f5c2823b8ee99d368c8c
AdrianTimpson <a.timpson@ucl.ac.uk>
2023-11-28 13:20:36 +0000

    specify dbname

--- R/functions.R
+++ R/functions.R
@@ -1,63 +1,18 @@
-
-
 #----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
-run.server.query <- function(sql.command){	
-
-	# create 'server.script.R' to be run on server
-	text <- c(
-		paste0("user <- '",user,"'"),
-		paste0("password <- '",password,"'"),
-		paste0("hostuser <- '",hostuser,"'"),
-		"source('https://raw.githubusercontent.com/BIADwiki/BIADwiki/main/R/functions.R')",
-		paste('sql.command <- c("',paste(sql.command,collapse='","'),'")',sep=''),
-		"query <- query.database(user, password, sql.command)",
-		"save(query, file='tmp.RData')"
-		)
-	writeLines(text,con= 'server.script.R')
-
-	query <- run.server.query.inner(user, password, hostuser, hostname, pempath)
-return(query)}
+# various functions and objects for BIAD
 #----------------------------------------------------------------------------------------------------
-run.server.query.inner <- function(user, password, hostuser, hostname, pempath){ 
-	require(ssh)
-	tmp.path <- paste("tmp/tmp",runif(1),sep='')
-
-	# create bash commands to be run on server
-	commands <- c(
-		paste("cd",tmp.path),
-		"/Library/Frameworks/R.framework/Resources/bin/R CMD BATCH --no-save server.script.R tmp.Rout",
-		"cd .."
-		)
-
-	# ssh onto server, copy required files to server, tell server to run R, copy results back to local 
-	session <- ssh_connect(host=paste(hostuser,"@",hostname,sep=''), keyfile=pempath)
-	ssh_exec_wait(session, command = paste("mkdir",tmp.path))
-	scp_upload(session, files = "server.script.R" , to = tmp.path, verbose=FALSE)
-	unlink('server.script.R')
-	ssh_exec_wait(session, command = commands)
-	RData <- paste(tmp.path,"tmp.RData",sep="/")
-	scp_download(session, files = RData, to = getwd(), verbose=FALSE)
-	cond <- file.exists("tmp.RData")
-	if(cond){
-		load('tmp.RData')
-		unlink('tmp.RData')
-		}
-	if(!cond){
-		query <- NULL
-		warning('sql command failed')
-		}
-	ssh_exec_wait(session, command = paste("rm -r",tmp.path))
-	ssh_disconnect(session)
-
-return(query)}
 #----------------------------------------------------------------------------------------------------
-run.server.searcher <- function(table.name, primary.value){
+dbname <- 'BIAD'
+source("https://raw.githubusercontent.com/BIADwiki/BIADwiki/main/R/functions.database.connect.R")
+#----------------------------------------------------------------------------------------------------
+run.server.searcher <- function(table.name, primary.value, dbname){
 
 	text <- c(
 		paste0("user <- '",user,"'"),
 		paste0("password <- '",password,"'"),
 		paste0("hostuser <- '",hostuser,"'"),
+		paste0("dbname <- '",dbname,"'"),
 		"source('https://raw.githubusercontent.com/BIADwiki/BIADwiki/main/R/functions.R')",
 		paste("table.name <- '",table.name,"'",sep=''),
 		paste("primary.value <- '",primary.value,"'",sep=''),
@@ -69,46 +24,7 @@ run.server.searcher <- function(table.name, primary.value){
 	writeLines(text,con= 'server.script.R')
 	query <- run.server.query.inner(user, password, hostuser, hostname, pempath)
 return(query)}
-
-#--------------------------------------------------------------------------------------------------
-query.database.inner <- function(user, password, sql.command){
-	require(RMySQL)
-	drv <- dbDriver("MySQL")
-
-	# close any connections to the database
-	cons <- dbListConnections(MySQL())
-	for(con in cons)dbDisconnect(con)
-
-	# connect locally to the database
-	con <- dbConnect(drv, user=user, pass=password, dbname='BIAD', host = "127.0.0.1", port=3306)
-	dbSendStatement(con,"SET NAMES 'utf8'")
-
-	# query the database and tidy
-	for(n in 1:length(sql.command)) res <- dbSendStatement(con,sql.command[n])
-	query <- fetch(res, n= -1)
-	query <- encoder(query)
-
-	# close any connections to the database
-	cons <- dbListConnections(MySQL())
-	for(con in cons)dbDisconnect(con)
-return(query)}
-#--------------------------------------------------------------------------------------------------
-query.database <- function(user, password, sql.command){
-	query <- suppressWarnings(query.database.inner(user, password, sql.command))
-return(query)}
-#--------------------------------------------------------------------------------------------------
-encoder <- function(df){
-	if(nrow(df)==0) return(NULL)
-	names(df) <- iconv(names(df),from="UTF-8",to="UTF-8")
-	if(nrow(df)>0){
-		for(n in 1:ncol(df))
-		if(class(df[,n])=="character"){
-			df[,n] <- iconv(df[,n],from="UTF-8",to="UTF-8")
-			}
-		return(df)	
-		}
-	}
-#--------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
 create.markdown.for.single.table <- function(d.tables, d.cols, table.name){
 	
 	table.comment <- subset(d.tables, TABLE_NAME==table.name)$TABLE_COMMENT
@@ -295,7 +211,7 @@ remove.blank.columns.from.table <- function(table){
 	tb <- tb[,keep.i,drop=F]
 return(tb)}
 #----------------------------------------------------------------------------------------------------
-get.related.data <- function(table.name, primary.value, fnc='what the hell is this argument for??', user, password){
+get.related.data <- function(table.name, primary.value, fnc, user, password){
 
 	sql.command <- "SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA='BIAD'"
 	keys <- query.database(user, password, sql.command)
