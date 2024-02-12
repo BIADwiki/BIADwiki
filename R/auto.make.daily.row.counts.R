06b12aba16351b011b0c51e6ebef0152eda186a6
AdrianTimpson <a.timpson@ucl.ac.uk>
2024-02-12 17:22:19 +0000

    automake templates

--- R/auto.make.daily.row.counts.R
+++ R/auto.make.daily.row.counts.R
@@ -18,7 +18,7 @@ standard <- subset(d, TABLE_NAME%in%standard)
 standard <- subset(standard, TABLE_ROWS>10)
 
 lookup <- subset(d, TABLE_NAME%in%lookup)
-lookup <- subset(standard, TABLE_ROWS>1)
+lookup <- subset(lookup, TABLE_ROWS>1)
 
 create.markdown.for.table.content(standard, d.cols, file = '../../Gists/summary_stats/row_counts/row_counts.md')
 create.markdown.for.table.content(lookup, d.cols, file = '../../Gists/summary_stats/row_counts/row_counts.md')
