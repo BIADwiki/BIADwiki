6d0a41401f5e1c504f763ff73c1120ee256ca34d
AdrianTimpson <a.timpson@ucl.ac.uk>
2024-05-06 15:27:35 +0100

    auto update from server

--- R/auto.make.daily.phase.models.R
+++ R/auto.make.daily.phase.models.R
@@ -18,7 +18,7 @@ prior.matrix <- matrix(1,200,200); prior.matrix <- prior.matrix/sum(prior.matrix
 row.names(prior.matrix) <- seq(min(mu.range),max(mu.range),length.out=nrow(prior.matrix))
 colnames(prior.matrix) <- seq(min(sigma.range),max(sigma.range),length.out=ncol(prior.matrix))
 
-N <- 1000
+N <- 100
 for(n in 1:N){
 
 	# pick a random phase
