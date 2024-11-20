b3cc865fc2715bf24c03d82974bd5a70f21225ad
Simon <simon@elisya.org>
2024-11-20 18:36:42 +0000

    first shot of rewrite auto.make tools

--- R/auto.make.daily.103.descriptions.R
+++ R/auto.make.daily.103.descriptions.R
@@ -4,12 +4,12 @@
 #-----------------------------------------------------------------------------------------
 
 # Pull all table meta data
-
+conn <- init.conn()
 sql.command <- "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='BIAD';"	
-d.tables <- query.database(user, password, 'biad', sql.command)	
+d.tables <- query.database(sql.command = sql.command, conn=conn)
 	
 sql.command <- "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema='BIAD';"	
-d.cols <- query.database(user, password, 'biad', sql.command)	
+d.cols <- query.database(sql.command = sql.command, conn=conn)
 
 # Group tables to put into different parts of the wiki:
 all <- d.tables$TABLE_NAME
@@ -21,4 +21,5 @@ standard <- all[!all%in%c(zprivate,zoptions,temp)]
 # construct a single markdown file for all standard tables and zoptions
 create.markdown.for.several.tables(d.tables, d.cols, table.names = standard, file = '../../Gists/table_comments/standard/standard.md')
 create.markdown.for.several.tables(d.tables, d.cols, table.names = zoptions, file = '../../Gists/table_comments/zoptions/zoptions.md')
+disconnect()
 #-----------------------------------------------------------------------------------------
