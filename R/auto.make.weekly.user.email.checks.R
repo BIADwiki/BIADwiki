bace68c7b3592aed94978ac326b4d2f250feb250
Simon <simon@elisya.org>
2024-11-20 19:03:26 +0000

    update weekly checks

--- R/auto.make.weekly.user.email.checks.R
+++ R/auto.make.weekly.user.email.checks.R
@@ -7,8 +7,9 @@
 #-----------------------------------------------------------------------------------------
 # check for users that are missing from the zprivate_users table
 #-----------------------------------------------------------------------------------------
-d1 <- query.database(user, password, 'biad', sql.command = "SELECT User FROM mysql.db WHERE Db='biad'")
-d2 <- query.database(user, password, 'biad', sql.command = "SELECT * FROM BIAD.zprivate_users")
+conn <- init.conn()
+d1 <- query.database(conn = conn, sql.command = "SELECT * FROM BIAD.zprivate_users")
+d2 <- query.database(conn = conn, sql.command = "SELECT CitationID, user_added FROM BIAD.citations")
 d <- merge(d1,d2,by.x='User',by.y='user',all=T)
 d <- subset(d, !User%in%c(user,'root','user'))
 
@@ -40,3 +41,4 @@ if(length(missing)>0){
 	gmailr::gm_send_message(email)
 	}
 #-----------------------------------------------------------------------------------------
+disconnect()
