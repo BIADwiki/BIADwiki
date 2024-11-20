0bf01c20a8dda63ce384f93b88fe62a6d9dfee58
Simon <simon@elisya.org>
2024-11-20 21:04:38 +0000

    add some exemple and comparison with the new search

--- R/example.searcher.R
+++ R/example.searcher.R
@@ -12,6 +12,11 @@ library(data.tree)
 x <- run.server.searcher(table.name = 'GraveIndividuals', primary.value = 'C03440') 
 x <- run.server.searcher(table.name = 'Sites', primary.value = 'S10050') 
 
+open.tunnel()
+
+x <- run.searcher(table.name = 'Sites', primary.value = 'S10050') 
+
+close.tunnel()
 # look at the data
 x
 
@@ -26,28 +31,32 @@ print(tree.up)
 # show the results as a plot
 plot(tree.down)
 plot(tree.up)
-
-# The same but with newer function get.relatives:
-conn <- init.conn()
-x <- get.relatives(table.name = 'GraveIndividuals', primary.value = 'C03440') 
-x <- get.relatives(table.name = 'Sites', primary.value = 'S10050') 
-disconnect()
-
-# look at the data
-x
-
-# look at direct relationships (tree) above or below
-tree.down <- FromListSimple(x$down)
-tree.up <- FromListSimple(x$up)
-
-# print the results as a list
-print(tree.down)
-print(tree.up)
-
-# show the results as a plot
-plot(tree.down)
-plot(tree.up)
-
-
-
 #----------------------------------------------------------------------------------------------------
+# Same with the new 
+
+open.tunnel()
+
+x <- get.relatives(table.name = 'Sites', primary.value = 'S10050') 
+
+close.tunnel()
+
+# look at the data
+x
+
+# look at direct relationships (tree) above or below
+tree.down <- FromListSimple(x$down)
+tree.up <- FromListSimple(x$up)
+
+# print the results as a list
+print(tree.down)
+print(tree.up)
+
+# show the results as a plot
+plot(tree.down)
+plot(tree.up)
+
+
+### more qdvance testing ---- 
+
+x <- {a=Sys.time();res <- get.relatives(table.name = 'Sites', primary.value = 'S10050', directions = "down") ; print(Sys.time()-a); res}
+x <- {a=Sys.time();res <- run.searcher(table.name = 'Sites', primary.value = 'S10050', direction = "down") ; print(Sys.time()-a); res}
