d52148dee892d3b9281a61c06176a5ddaa7af952
Simon <simon@elisya.org>
2024-11-20 19:05:59 +0000

    stopping push and pull of image

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -10,44 +10,44 @@ cd R
 R CMD BATCH --no-save controller.R
 cd ..
 
-# one last pull, then push the changes
-git status
-git pull
-git add -A
-git commit -m "auto update from server"
-git push
-git status
-
-# update the Gists
-cd ../Gists/table_comments
-
-cd standard
-git status
-git pull
-git add -A
-git commit -m "auto update from server"
-git push
-git status
-cd ..
-
-cd zoptions
-git status
-git pull
-git add -A
-git commit -m "auto update from server"
-git push
-git status
-cd ..
-
-
-cd ..
-cd summary_stats
-
-cd row_counts
-git status
-git pull
-git add -A
-git commit -m "auto update from server"
-git push
-git status
-cd ..
+## one last pull, then push the changes
+#git status
+#git pull
+#git add -A
+#git commit -m "auto update from server"
+#git push
+#git status
+#
+## update the Gists
+#cd ../Gists/table_comments
+#
+#cd standard
+#git status
+#git pull
+#git add -A
+#git commit -m "auto update from server"
+#git push
+#git status
+#cd ..
+#
+#cd zoptions
+#git status
+#git pull
+#git add -A
+#git commit -m "auto update from server"
+#git push
+#git status
+#cd ..
+#
+#
+#cd ..
+#cd summary_stats
+#
+#cd row_counts
+#git status
+#git pull
+#git add -A
+#git commit -m "auto update from server"
+#git push
+#git status
+#cd ..
