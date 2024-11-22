ba4530238b7c661ba02bf2a60ecbf1dd4acf91f2
Simon <simon@elisya.org>
2024-11-22 10:30:53 +0000

    correct tool path

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -13,8 +13,9 @@ Rscript controller.R > controller.Rout_$DATE
 ## this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
 scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/controller_last.txt
 scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/
-scp -r -F /dev/null -P 2222 tools/templates/ root@biadwiki.org:/media/biad/
 cd ..
+
+scp -F /dev/null -P 2222 -r tools/templates/ root@biadwiki.org:/media/biad/
 
 # one last pull, then push the changes
 git status
