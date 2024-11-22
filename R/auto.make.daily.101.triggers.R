9cc9fcd5ed84dd4325143ad6d9186963df8c5600
Simon <simon@elisya.org>
2024-11-22 14:20:38 +0000

    fix problem with triger (drop.trigger will alwes be >0, need to chec old.trigger to skip)

--- R/auto.make.daily.101.triggers.R
+++ R/auto.make.daily.101.triggers.R
@@ -13,7 +13,7 @@ prefix <- 'auto_pad_'
 old.triggers <- trig[substr(trig, 1, nchar(prefix)) ==prefix]
 drop.triggers <- paste('DROP TRIGGER `',old.triggers,'`;',sep='')
 new.triggers <- make.all.triggers(all, prefix, trigger = make.autopad.trigger)
-if(length(drop.triggers)>0)query.database(sql.command = drop.triggers, conn = conn)
+if(length(old.triggers)>0)query.database(sql.command = drop.triggers, conn = conn)
 query.database(sql.command = new.triggers, conn = conn)
 
 # time/user stamp triggers
@@ -22,7 +22,7 @@ old.triggers <- trig[substr(trig, 1, nchar(prefix)) ==prefix]
 drop.triggers <- paste('DROP TRIGGER `',old.triggers,'`;',sep='')
 sub <- subset(all, COLUMN_NAME=='time_added')
 new.triggers <- make.all.triggers(sub, prefix, trigger = make.stamp.trigger)
-if(length(drop.triggers)>0)query.database(sql.command = drop.triggers,conn=conn)
+if(length(old.triggers)>0)query.database(sql.command = drop.triggers,conn=conn)
 query.database(sql.command = new.triggers,conn=conn)
 disconnect()
 #--------------------------------------------------------------------------------------------------------------
