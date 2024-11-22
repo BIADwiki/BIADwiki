ca0c22c9565839418ad01115b86c386e997fd31e
Simon <simon@elisya.org>
2024-11-22 10:24:16 +0000

    save history of logs oc controller.Rout, in prevision of #15

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -5,10 +5,15 @@ PATH="$PATH:/usr/local/bin/"
 git status
 git pull
 
+DATE=$(date +%Y%m%d)
+#
 # run R scripts
 cd R
-Rscript controller.R  > controller.Rout
-scp -F /dev/null -P 2222 controller.Rout root@biadwiki.org:/media/biad/controller.txt
+Rscript controller.R > controller.Rout_$DATE
+## this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
+scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/controller_last.txt
+scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/
+scp -r -F /dev/null -P 2222 tools/templates/ root@biadwiki.org:/media/biad/
 cd ..
 
 # one last pull, then push the changes
