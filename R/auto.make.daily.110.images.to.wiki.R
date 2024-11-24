fe608f7804317710bb2cdd27d7a585928604acb8
Simon <simon@elisya.org>
2024-11-24 03:10:47 +0000

    send csv, md, html,etc... to the wiki

--- R/auto.make.daily.110.images.to.wiki.R
+++ R/auto.make.daily.110.images.to.wiki.R
@@ -9,7 +9,7 @@ require(tools)
 token <- readLines('../tools/API/API.txt')
 
 # set headers
-headers <- add_headers(Authorization = paste('Bearer',token))
+headers <- httr::add_headers(Authorization = paste('Bearer',token))
 
 # URL for the POST request
 url <- 'https://biadwiki.org/u'
@@ -17,23 +17,38 @@ url <- 'https://biadwiki.org/u'
 # get image filepaths and types
 path <- list.files('../tools/plots', full.names=TRUE)
 type <- paste('image',file_ext(path),sep='/')
+path <- c(list.files('../tools/templates', full.names=TRUE),path)
+type <- c(paste('csv',file_ext(path),sep='/'),type)
+path <- c(list.files('../tools/table_comments', full.names=TRUE),path)
+type <- c(paste('html',file_ext(path),sep='/'),type)
+path <- c(list.files('../tools/summary_stats', full.names=TRUE),path)
+type <- c(paste('html',file_ext(path),sep='/'),type)
 
 # loop for each image file
 N <- length(path)
+print(paste("sending",N,"files to wiki"))
 for(n in 1:N){
+    prog  <-  paste0("sending file: ", n, "/", N, " [", basename(path[n]), "]")
+    cat('\r',sprintf("%-*s", 80, prog), sep="")
 
-	# Set up the body of the request
-	body <- list(
-		mediaUpload = upload_file(path = path[n], type = type[n]),
-		mediaUpload = '{"folderId":0}'
-		)
+    tryCatch({
+        # Set up the body of the request
+        body <- list(
+                     mediaUpload = httr::upload_file(path = path[n], type = type[n]),
+                     mediaUpload = '{"folderId":0}'
+        )
 
-	# Make the POST request
-	result <- POST(url, headers, body = body)
+        Sys.sleep(.1)
+        # Make the POST request
+        result <- httr::POST(url, headers, body = body)
 
-	# Print the result if problematic
-	res <- content(result, "text")
-	if(res!='ok')print(res)
+        # Print the result if problematic
+        res <- httr::content(result, "text")
+        if(res!='ok')print(res)
+    },error=function(err){
+        print(err)
+    })
 	}
+cat("\n")
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
