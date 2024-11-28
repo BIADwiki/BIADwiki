3d85e5c2b9a3bacea68a2169c259ff4dc396ad41
Simon <simon@elisya.org>
2024-11-28 08:52:30 +0000

    minor adjust of scps

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -12,13 +12,13 @@ cd R
 Rscript controller.R > controller.Rout_$DATE
 ## this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
 scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/controller_last.txt
-scp -F /dev/null -P 2222 controller.Rout_$DATE root@biadwiki.org:/media/biad/
 mv controller.Rout_$DATE ../tools/logs/
 cd ..
 
 scp -F /dev/null -P 2222 -r tools/templates/ root@biadwiki.org:/media/biad/
 scp -F /dev/null -P 2222 -r tools/summary_stats/ root@biadwiki.org:/media/biad/
 scp -F /dev/null -P 2222 -r tools/table_comments/ root@biadwiki.org:/media/biad/
+scp -F /dev/null -P 2222 -r tools/logs/ root@biadwiki.org:/media/biad/logs
 
 # one last pull, then push the changes
 git status
