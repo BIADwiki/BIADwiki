97456a2b4cff4e06287bed81e4d317667b6bd26a
BIADwiki <BIAD.committee@gmail.com>
2025-04-01 22:39:24 +0100

    correct BIADIWKI.sh to use biad.cloud and to NOT commit all

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -11,20 +11,20 @@ DATE=$(date +%Y%m%d)
 cd R
 Rscript controller.R > controller.Rout_$DATE
 ## this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
-scp -P 2222 controller.Rout_$DATE tunnel@biadwiki.org:/media/biad/controller_last.txt
-mv controller.Rout_$DATE ../tools/logs/
+scp -P 2222 controller.Rout_$DATE tunnel@biad.cloud:/media/biad/controller_last.txt
+mv controller.Rout_$DATE ../../logs/
 cd ..
 
 # backup files in dropplet scp tunnel
 for fold in logs templates summary_stats table_comments ;
 do
-    rsync -avz -e "ssh -p 2222" tools/$fold tunnel@biadwiki.org:/media/biad/
+    rsync -avz -e "ssh -p 2222" tools/$fold tunnel@biad.cloud:/media/biad/
 done
 
 # one last pull, then push the changes
-git status
-git pull
-git add -A
-git commit -m "auto update from server"
-git push
-git status
+#git status
+#git pull
+#git add -A
+#git commit -m "auto update from server"
+#git push
+#git status
